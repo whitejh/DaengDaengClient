7122cc18cf582db0166b7e6f9bb1d52e5cc0153d
Gyu <melon9751@naver.com>
2021-06-21 19:40:20 +0900

    [feat] 상품 저장소에 조회 추가

--- BE/src/main/java/com/dignity/puppymarket/repository/ItemRepository.java
+++ BE/src/main/java/com/dignity/puppymarket/repository/ItemRepository.java
@@ -4,7 +4,10 @@ import com.dignity.puppymarket.domain.Item;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
     List<Item> findAll();
+    
+    Optional<Item> findById(Long id);
 }
