bc3fd0d45d3a5c19562d70b5234f3d2bda3f9b27
hackjap <hackjap1015@gmail.com>
2021-06-23 20:52:22 +0900

    :art:mysql, json-server 폴더 추[B[B가 및 무한 스크롤 기능 구현

--- front-vue2/src/store/index.js
+++ front-vue2/src/store/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-<<<<<<< HEAD
 import state from './state/index.js';
 import mutations from './mutations.js';
 import actions from './actions.js';
 import getters from './getters.js';
-=======
-import state from "./state/index.js";
-import mutations from './mutations.js';
-import actions from './actions.js';
->>>>>>> 3c87b0a6021a5c6d542de4e4343c2d376f33ffd6
 //import ES6Promise from 'es6-promise';
 
 //ES6Promise.polyfill();
@@ -19,13 +13,7 @@ export const store = new Vuex.Store({
 	state,
 	mutations,
 	actions,
-<<<<<<< HEAD
 	getters,
 });
 
 export default store;
-=======
-});
-
-export default store;
->>>>>>> 3c87b0a6021a5c6d542de4e4343c2d376f33ffd6
--- front-vue2/src/store/state/index.js
+++ front-vue2/src/store/state/index.js
@@ -1,19 +1,12 @@
 import stateMenu from './menus.js';
 import stateColor from './colors.js';
 import stateTest from './test.js';
-<<<<<<< HEAD
 import stateAuth from './auth.js';
-=======
-
->>>>>>> 3c87b0a6021a5c6d542de4e4343c2d376f33ffd6
 const state = {
 	...stateMenu,
 	...stateColor,
 	...stateTest,
-<<<<<<< HEAD
 	...stateAuth,
-=======
->>>>>>> 3c87b0a6021a5c6d542de4e4343c2d376f33ffd6
 };
 
 export default state;
